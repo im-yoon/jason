63dd814356fe28229e5ef690da66280d219c2e1d
im-yoon <syuu1219@gmail.com>
2021-01-14 17:15:22 +0900

    update

--- js/jason.js
+++ js/jason.js
@@ -15,9 +15,15 @@
             var that = null;
             var _window = $(window);
             var _header = $('#header');
+            var _nav = $('#nav');
+            var mainBtn = $('.mainBtn');
+            var menuBar = $('.menu-bar');
+            var _sub    = $('.sub');
             var _scroll = false; 
-            var t = false;  // true =0
-            
+            var t       = false;  // true =0
+            var m = 0; //메뉴클릭 안한 상태
+            var s = -1; //부호 기본값 양수 -1
+            var topPosi = 124;
                 
                 _header.on({
                     mouseenter:function(){
@@ -26,7 +32,7 @@
                     },
                     mouseleave:function(){
                         that = $(this);
-                        if( _scroll === false ){
+                        if( _scroll === false && m === 0){ //두조건 모두 만족 시 해더 배경 없어짐
                             that.removeClass('addHeader');
                         }
                         
@@ -48,7 +54,89 @@
                     else{
                         t=false; //30이하 로 올하 갔을때 false가되서 또 실행가능
                         _scroll = false; //스크롤 30px 이하인 경우 false 변경
-                        _header.removeClass('addHeader');
+                        if( m===0 ){ //햄버거메뉴 클릭안된 상태만 헤더 배경 삭제
+                            _header.removeClass('addHeader');
+                        }
+                        
+                    }
+                });
+
+                
+                _window.resize(function(){
+                    resizeFn();
+                });
+
+                //화면 크기에 변화에 따른 반응형 데스크탑,태블릿과 모바일 메뉴
+                function resizeFn(){
+                    if($(this).innerWidth()>1024){
+                        topPosi = 124;
+                        //$('.sub').stop().slideUp(0);
+                        _nav.stop().show(0).animate({top:(s*topPosi)},300);
+                    }
+                    else if($(this).innerWidth()>780){
+                        topPosi = 84;
+                        //$('.sub').stop().slideUp(0);
+                        _nav.stop().show(0).animate({top:(s*topPosi)},300);
+                    }
+                    else{
+                        topPosi = 0;
+                        _sub.stop().slideDown(0);
+                        _nav.stop().animate({top:0},0);
+                        if(m==1){
+                            _nav.stop().show(0);
+                            $('html').addClass('addScroll');
+                        }
+                        else{
+                            _nav.stop().hide(0);
+                            $('html').removeClass('addScroll');
+                        }
+  
+                    
+                    }
+                    
+                }
+                _nav.hide(0);
+                setTimeout(resizeFn,10);
+                
+               //햄버거 메뉴 클릭하면 기억하는 변수 설정
+               //NAV 네비게이션 이벤트
+               menuBar.on({
+                    click : function(e){
+                        e.preventDefault();
+                        if(m==0){ //처음 클릭(토글)
+                            m = 1;
+                            s = 1; //부호 + 양수
+                           
+                        }
+                        else{ //두번째 클릭(토글)
+                            m=0;
+                            s=-1; //부호 - 음수
+                            
+                        }
+                        resizeFn(); //버튼 클릭시 변화를 주는 반응형 함수
+                        $(this).toggleClass('addBtn');
+                        
+                        
+
+                    }
+                });
+                
+                //메인버튼 이벤트
+                mainBtn.on({
+                    mouseenter: function(){
+                        if(_window.innerWidth()>780){
+                            _sub.stop().slideUp(100);
+                            $(this).next('.sub').stop().slideDown(300);
+                        }
+                        
+                    }
+                });
+
+                //서브 메뉴 사라지는 효과 이벤트
+                //NAV를 벗어나면 사라짐
+                _nav.on({
+                    mouseleave: function(){
+                        _sub.stop().slideUp(300); 
                     }
                 });
 
@@ -413,6 +501,125 @@
 
         },
         section4Fn:function(){
+
+            //슬라이드 콘테이너 박스 너비에 따른 슬라이드 3개의 너비구하기
+            //1570-40(마진값20*2)=1530
+            //1.슬라이드너비는 1570/3=523.3333333
+            //반응형으로 슬라이드 컨테이너('.slide-container) 박스 너비 변화에 따른 슬라이드 너비 게산
+
+            var totN       = $('#section4 .slide').length; //10
+            var slideN     = 3;//desktop 980초과 3, tablet 980 이하 2,mobile 600 1 
+            var $slideCon  = $('#section4 .slide-container');
+            var slideW     = $('#section4 .slide-container').innerWidth()/slideN;
+            var $slideWrap = $('#section4 .slide-wrap');
+            var $slide     = $('#section4 .slide'); 
+            var $pageBtn     = $('#section4 .pageBtn'); 
+            var $window    = $(window);
+            var cnt        = 0;
+            var setId      = null;
+            var setId2      = null;
+
+                function resizeFn(){
+                    if($slideCon.innerWidth()>1024){
+                        slideN = 3;
+                    }
+                    else if($slideCon.innerWidth()>680){
+                        slideN = 2;
+                    }
+                    else{
+                        slideN = 1;
+                    }
+                    slideW = $slideCon.innerWidth()/slideN; //슬라이드 1개의 너비
+                    $slideWrap.css({width:(slideW*totN),marginLeft:-(slideW*3)}); //3 4 3
+                    $slide.css({width:slideW,height:slideW-40});
+                    $slideWrap.stop().animate({left:-(slideW*cnt)},0);//동적:속도를 써서해도 되고 메인함수 호출 해도 됨 / 정적:움직임 없이 하기 위해 0
+                }
+                setTimeout(resizeFn,10); //처음 로딩 시 1번만
+                
+                $window.resize(function(){ //크기가 변경 될때만 반응
+                    resizeFn();
+                });
+
+                //1. 메인슬라이드 함수
+                function mainSlideFn(){
+                    $slideWrap.stop().animate({left:-(slideW*cnt)},600,'easeOutExpo',function(){
+                        if(cnt>3){cnt=0}; //0~3 (4개의 슬라이드)
+                        if(cnt<0){cnt=3};
+                        $slideWrap.stop().animate({left:-(slideW*cnt)},0);
+                    });
+                    pageBtnEventFn();
+                }
+                //2. 다음 카운트 슬라이드 
+                function nextCountSlideFn(){
+                    cnt++;
+                    mainSlideFn();
+                }
+                //2.2 이전 카운트 슬라이드 함수
+                function prevCountSlideFn(){
+                    cnt--;
+                    mainSlideFn();
+                }
+
+                //3. 스와이프 다음/이전 터치 이벤트
+                $slideCon.swipe({
+                    swipeLeft:function(){
+                        timerControlFn();
+                        if(!$slideWrap.is(':animated')){ //이벤트가 진행 중이지 않다면 nextCountSlideFn
+                            nextCountSlideFn();
+                        }
+                        
+                    },
+                    swipeRight:function(){
+                        timerControlFn();
+                        if(!$slideWrap.is(':animated')){
+                            prevCountSlideFn();
+                        }
+                        
+                    }
+                });
+
+                //4.페이지버튼 이벤트 함수
+                function  pageBtnEventFn(){
+                    var z = cnt;
+                    if(z>3){z=0}
+                    if(z<0){z=3}
+                    $pageBtn.removeClass('addPage');
+                    $pageBtn.eq(z).addClass('addPage');
+                }
+                //5.페이지버튼 클릭 이벤트
+                $pageBtn.each(function(idx){
+                    $(this)
+                        .on('click',function(e){
+                            e.preventDefault();
+                            timerControlFn();
+                            cnt = idx; //직접 선택한 슬라이드 번호를 이용 메인 슬라이드 함수 호출
+                            mainSlideFn();
+                        });
+
+                });
+                
+
+                //6.자동 실행 타이머 6초간격
+                function autoPlayFn(){
+                    setId =setInterval(nextCountSlideFn,6000);
+                }
+
+                autoPlayFn();
+
+                //7. 타이머 컨트롤 함수
+                function timerControlFn(){
+                    var tcnt = 0 ;
+                    clearInterval(setId);
+                    clearInterval(setId2);
+                    setId2 = setInterval(function(){
+                        tcnt++;
+                        if(tcnt>=6){
+                            clearInterval(setId2);
+                            nextCountSlideFn();
+                            autoPlayFn();
+                        }
+                    },1000);
+                }
 
         },
         section5Fn:function(){
